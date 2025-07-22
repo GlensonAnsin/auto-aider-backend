c22d33f9ad42a2d5d8c70954d0c482d59890dbc5
GlensonAnsin <ansin.glenson01@gmail.com>
2025-07-23 01:22:35 +0800

    feat: added column in mechanic request table

--- src/models/mechanicRequestModel.js
+++ src/models/mechanicRequestModel.js
@@ -3,11 +3,12 @@ import { sequelize } from '../config/postgresDB.js';
 
 const MechanicRequest = sequelize.define('mechanic_request', {
     mechanic_request_id: { type: DataTypes.BIGINT, autoIncrement: true, primaryKey: true },
-    vehicle_diagnostic_id: { type: DataTypes.BIGINT, allowNull: false },
+    vehicle_diagnostic_id: { type: DataTypes.BIGINT },
     repair_shop_id: { type: DataTypes.BIGINT, allowNull: false },
     repair_procedure: { type: DataTypes.TEXT, allowNull: false },
     request_datetime: { type: DataTypes.DATE, allowNull: false },
     status: { type: DataTypes.STRING(20), allowNull: false },
+    vehicle_name: { type: DataTypes.STRING },
 }, {
     tableName: 'mechanic_request',
     timestamps: false,
