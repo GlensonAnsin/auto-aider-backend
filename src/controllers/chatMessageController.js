844ff33acae5b1e36ce9e34fda1fda5931eba9fe
GlensonAnsin <ansin.glenson01@gmail.com>
2025-08-31 03:49:19 +0800

    feat: add function for inbox

--- src/controllers/chatMessageController.js
+++ src/controllers/chatMessageController.js
@@ -51,6 +51,47 @@ export const getConversationForShop = async (req, res) => {
   }
 };
 
+// GET ALL CONVERSATIONS FOR CAR OWNER
+export const getAllConversationsCO = async (req, res) => {
+  const user_id = req.user.user_id;;
+  
+  try {
+    const user = await User.findOne({ where: { user_id: user_id } });
+
+    if (user) {
+      const allChats = await ChatMessage.findAll({
+        where: {
+          [Op.or]: [
+            { sender_user_id: user_id },
+            { receiver_user_id: user_id },
+          ]
+        }
+      });
+      res.status(200).json(allChats);
+    }
+
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+};
+
+// GET ALL CONVERSATIONS FOR REPAIR SHOP
+export const getAllConversationsRS = async (req, res) => {
+  const repair_shop_id = req.user.repair_shop_id;
+  
+  try {
+    const shop = await AutoRepairShop.findOne({ where: { repair_shop_id: repair_shop_id } });
+
+    if (shop) {
+      const allChats = await ChatMessage.findAll({ where: { sender_repair_shop_id: repair_shop_id } });
+      res.status(200).json(allChats);
+    }
+
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+};
+
 // SEND AND RECEIVE MESSAGE
 export const sendMessage = async (req, res) => {
   const { senderID, receiverID, role, message, sentAt } = req.body;
--- src/routes/chatMessageRoutes.js
+++ src/routes/chatMessageRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
-import { getConversationForCarOwner, getConversationForShop, sendMessage } from '../controllers/chatMessageController.js';
+import { getConversationForCarOwner, getConversationForShop, sendMessage, getAllConversationsCO, getAllConversationsRS } from '../controllers/chatMessageController.js';
 
 const router = express.Router();
 
 router.get('/get-conversation-co/:repair_shop_id', authMiddleware, getConversationForCarOwner);
 router.get('/get-conversation-rs/:user_id', authMiddleware, getConversationForShop);
+router.get('/get-all-chats-co', authMiddleware, getAllConversationsCO);
+router.get('/get-all-chats-rs', authMiddleware, getAllConversationsRS);
 router.post('/send-message', sendMessage);
 
 export default router;
